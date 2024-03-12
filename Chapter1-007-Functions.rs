52822017bdc6840e02f27847de48bd1149620a97
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-12 23:32:43 +0300

    Create Chapter1-007-Functions.rs

---
+++ Chapter1-007-Functions.rs
@@ -0,0 +1,15 @@
+fn calculate_area_rectangular(x: u32, y: u32) -> u32 {
+    x * y
+}
+
+fn main() {
+    println!("Area 1 = {}", calculate_area_rectangular(21, 13));
+    println!("Area 2 = {}", calculate_area_rectangular(12, 14));
+}
+
+/*
+
+Area 1 = 273
+Area 2 = 168
+
+*/
