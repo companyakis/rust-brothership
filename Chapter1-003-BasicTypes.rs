9f92c546dfe48d3aba117fab9b9b19f8428197ea
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-12 23:10:29 +0300

    Create Chapter1-003-BasicTypes.rs

---
+++ Chapter1-003-BasicTypes.rs
@@ -0,0 +1,36 @@
+fn main() {
+
+    let my_name: &str = "Mustafa";
+    
+    let pi: f32 = 3.14;
+    
+    let are_you_ready: bool = true;
+    
+    println!("{}", my_name);
+    println!("{}", pi);
+    println!("{}", are_you_ready);
+
+}
+
+/*
+bool -> true/false
+
+unsigned integers -> u8 u16 u32 u64 u128
+
+signed integers -> i8 i16 i32 i64 i128
+
+pointer sized integers -> usize isize
+
+floating point -> f32 f64
+
+characters -> char
+
+tuple -> (value, value, ...)
+
+arrays -> [value, value, ...]
+
+slices -> a collection of similar elements with length known at runtime
+
+str(string slice) -> text with a length known at runtime
+
+*/
