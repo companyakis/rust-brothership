3284e0be90cdb0bc5251852b61545db38194210d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-12 23:18:05 +0300

    Create Chapter1-005-Constants.rs

---
+++ Chapter1-005-Constants.rs
@@ -0,0 +1,9 @@
+//Unlike variables, constants must always have explicit types!!!
+
+const STARTING_YEAR: u16 = 1923;
+
+fn main() {
+
+    println!("{} is very important!", STARTING_YEAR); //1923 is very important!
+
+}
