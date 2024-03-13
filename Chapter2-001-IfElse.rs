eafe449f2a1959b486626d2f9527e68d01e530b5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-13 10:02:40 +0300

    Create Chapter2-001-IfElse.rs

---
+++ Chapter2-001-IfElse.rs
@@ -0,0 +1,15 @@
+fn main() {
+    
+    let year: u16 = 2024;
+    
+    if year <= 2025 && year > 2018 {
+        println!("I hope we have time!");
+    }
+    
+    else if year == 2018 {
+        println!("What an awful year!");
+    }
+    else {
+        println!("Nice past or unknown future...");
+    }
+}
